8b05dc402ed5631dcbae9f8da9d11a601b77beb3
Chughtai <MCHUGHT1@ford.com>
2018-10-09 22:28:50 -0400

    Separated controllers for scan items by number and by weight and refactored ScanItemController

--- src/main/java/com/ordertotal/controllers/ScanItemController.java
+++ src/main/java/com/ordertotal/controllers/ScanItemController.java
@@ -4,7 +4,6 @@ import com.ordertotal.model.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.*;
 
 @RestController
@@ -16,33 +15,36 @@ public class ScanItemController {
         public List scanEachesItem(@PathVariable("item") Items scannedItem) {
         Cart cart = Cart.getInstance();
 
-        if (cart.itemsInCart.containsKey(scannedItem)){
-            ScannedItemCheckoutDetail existingItemDetail = getExistingItemCountAndTotalItemPriceFromCart(scannedItem, cart);
-            addScannedItemToExistingItemDetail(scannedItem,existingItemDetail,cart);
+        if (cartAlreadyHasTheItem(scannedItem, cart)){
+            addScannedItemToExistingScannedItemsMap(scannedItem,cart);
         }
         else{
-            ScannedItemCheckoutDetail itemDetail = new ScannedItemCheckoutDetail(1, scannedItem.price);
-            cart.itemsInCart.put(scannedItem, itemDetail);
+            addNewItemToCart(scannedItem, cart);
         }
 
         ResponseEntity response = new ResponseEntity();
         return response.getServiceResponse();
     }
 
-
-
-    private ScannedItemCheckoutDetail getExistingItemCountAndTotalItemPriceFromCart(@PathVariable("item") Items scannedItem, Cart cart) {
-        int currentCountOfThisScannedItem = cart.itemsInCart.get(scannedItem).getNumberOfItems();
-        BigDecimal currentTotalPriceOfThisScannedItem = cart.itemsInCart.get(scannedItem).getTotalItemPrice();
-
-        return new ScannedItemCheckoutDetail(currentCountOfThisScannedItem, currentTotalPriceOfThisScannedItem);
+    private boolean cartAlreadyHasTheItem(@PathVariable("item") Items scannedItem, Cart cart) {
+        return cart.itemsInCart.containsKey(scannedItem);
     }
 
-    private void addScannedItemToExistingItemDetail(Items scannedItem, ScannedItemCheckoutDetail existingItemDetail, Cart cart){
-        BigDecimal scannedItemPrice = scannedItem.price;
-        ScannedItemCheckoutDetail newItemDetail = new ScannedItemCheckoutDetail(existingItemDetail.getNumberOfItems()+1,
-                                            existingItemDetail.getTotalItemPrice().add(scannedItemPrice));
+    private void addScannedItemToExistingScannedItemsMap(Items scannedItem, Cart cart) {
+        int currentCountOfThisScannedItem = cart.itemsInCart.get(scannedItem).getNumberOfItems();
+        BigDecimal currentTotalPriceOfThisScannedItem = cart.itemsInCart.get(scannedItem).getTotalItemPrice();
+        updateItemCountAndPrice(scannedItem,currentCountOfThisScannedItem, currentTotalPriceOfThisScannedItem, cart);
+    }
+
+    private void updateItemCountAndPrice(Items scannedItem, int currentCountOfThisScannedItem, BigDecimal currentTotalPriceOfThisScannedItem, Cart cart){
+        ScannedItemCheckoutDetail newItemDetail = new ScannedItemCheckoutDetail(currentCountOfThisScannedItem+1,
+                currentTotalPriceOfThisScannedItem.add(scannedItem.price));
         cart.itemsInCart.put(scannedItem, newItemDetail);
+    }
+
+    private void addNewItemToCart(Items scannedItem, Cart cart) {
+        ScannedItemCheckoutDetail itemDetail = new ScannedItemCheckoutDetail(1, scannedItem.price);
+        cart.itemsInCart.put(scannedItem, itemDetail);
     }
 
 }
